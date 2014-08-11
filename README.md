c29b2ee4112659fc932dd253ba2855d33a65ea3d
johnfitzpatrick <jhnftzptrck@gmail.com>
2014-08-11 12:54:21 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# AAR
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'AAR'
+maintainer       ''
+maintainer_email ''
+license          ''
+description      'Installs/Configures AAR'
+long_description 'Installs/Configures AAR'
+version          '0.1.0'
+
